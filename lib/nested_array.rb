64cb9445a69d71ccf4bc1efd1d8fcb542829c6dc
Mohammed shaikh <mqshaikh8@gmail.com>
2019-11-04 16:51:56 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -20,7 +20,7 @@ def assembled_matrix
   # Build an array that contains (or, "nests") the residents of The Ennet House
   # and the Enfield Tennis Academy as provided by the constants
   #nested array = [["Don Gately","Joelle van Dyne","Pat M.","Kate Gompert","Bruce Green"],["Hal Incandenza","Lyle","Gerhard Schtitt","Mario Incandenza","Michael Pemulis"]]
-  ENNET_HOUSE + ENFIELD_TENNIS_ACADEMY
+  ENNET_HOUSE.concatonate(ENFIELD_TENNIS_ACADEMY)
   
 end
 
